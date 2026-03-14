2e457b73d1750b580cb1b6e8d455762f99503208
MahendraMankar <mahendramankar98@gmail.com>
2026-03-14 19:00:53 +0530

    Learn JavaScript Methods DisabledElement ScrolBy ScrollTo ScrollIntoView Method added

---
+++ src/test/java/advanceProgram/JavaScriptScrollBy.java
@@ -0,0 +1,25 @@
+package advanceProgram;
+
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaScriptScrollBy {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://doodles.google/");
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		//scrollBy() method
+		js.executeScript("window.scrollBy(0,1000)");
+		Thread.sleep(2000);
+		js.executeScript("window.scrollBy(0,2000)");
+		
+	}
+
+}
