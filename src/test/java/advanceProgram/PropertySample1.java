ca141f37ebfac365177c85ee0f54685aac4b2e01
MahendraMankar <mahendramankar98@gmail.com>
2026-03-24 12:37:40 +0530

    learn PropertySample program and commondata.property file

--- src/test/java/advanceProgram/PropertySample1.java
+++ src/test/java/advanceProgram/PropertySample1.java
@@ -2,19 +2,31 @@ package advanceProgram;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Properties;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PropertySample1 {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("src\\test\\resources\\commondata.properties");
+        Properties prop = new Properties();
+        prop.load(fis);
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		FileInputStream fis = new FileInputStream("src\\test\\resources\\commondata.properties");
-		Properties prop = new Properties();
-		prop.load(fis);
-		String URL = prop.getProperty("url");
-		String UN = prop.getProperty("username");
-		String PWD = prop.getProperty("password");
-		
-	}
+        String URL = prop.getProperty("url");
+        String UN = prop.getProperty("username");
+        String PWD = prop.getProperty("password");
 
+        System.out.println(URL);
+        System.out.println(UN);
+        System.out.println(PWD);
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get(URL);
+        driver.findElement(By.name("email")).sendKeys(UN);
+        driver.findElement(By.name("pass")).sendKeys(PWD);
+    }
 }
